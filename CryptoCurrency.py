0d55a31f7359f9a46728ea008937283e5b926588
Akash Bhardwaj <48449366+akashbhardwaj04@users.noreply.github.com>
2020-04-15 00:39:20 +0530

    New libraries with Add-transaction function

--- CryptoCurrency.py
+++ CryptoCurrency.py
@@ -5,8 +5,10 @@
 import hashlib
 import datetime
 import json
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+import requests
+from uuid import uuid4
+from urllib.parse import urlparse
 
 
 #----------------------------------Generating a blockchain---------------------------------
@@ -14,6 +16,7 @@ from flask import Flask, jsonify
 class Blockchain:
     def __init__(self):
         self.chain = []
+        self.transactions = []
         self.create_block(proof=1, previous_hash='0')
     
     def create_block(self, proof, previous_hash):
@@ -21,7 +24,9 @@ class Blockchain:
                   'timestamp': str(datetime.datetime.now()),
                  'proof': proof,
                  'previous_hash': previous_hash,
-                }
+                 'transactions': self.transactions
+                 }
+        self.transactions = []
         self.chain.append(block)
         return block
     
@@ -59,6 +64,13 @@ class Blockchain:
             block_index +=1
         return True
     
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({'sender': sender,
+                                  'receiver': receiver,
+                                  'amount': amount
+                                  })
+        previous_block = self.get_previous_block()
+        return previous_block['index'] +1
         
 
 #-------------------------------Creating a webApp------------------------------------------
